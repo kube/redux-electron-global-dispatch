a3762da81a374bae1d243f60c27dbcd99366cb43
Chris Feijoo <christophe@feijoo.fr>
2017-02-08 02:36:28 +0100

    Default export for both Main and Renderer Process

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
 import { Action, Middleware, MiddlewareAPI, Dispatch } from 'redux'
-import { ipcRenderer, ipcMain } from 'electron'
+import { ipcRenderer, ipcMain, BrowserWindow } from 'electron'
+
+const GLOBAL_ACTION_MESSAGE = 'GLOBAL_REDUX_ACTION'
 
 type GlobalAction = Action & {
   type: string
   global?: true
 }
 
-const GLOBAL_ACTION_MESSAGE = 'GLOBAL_REDUX_ACTION'
+const isRenderer = process.type === 'renderer'
 
 /**
- * Detects if a Redux Action is meant to be dispatched globally
+ * Detects if an Action is global
  */
-const isActionGlobal = (action: GlobalAction) =>
+const isGlobalAction = (action: GlobalAction) =>
   action.global === true
 
 /**
- * Return a copy of given Action with global attribute removed
- * to dispatch it without being intercepted once again by the middleware
+ * Unset global attribute on Action to dispatch it
+ * without being intercepted once again by the middleware
  */
 const extractAction = (action: GlobalAction): Action => {
   const localAction = { ...action }
@@ -25,41 +27,37 @@ const extractAction = (action: GlobalAction): Action => {
 }
 
 /**
- * Called from Main Process to dispatch Action to all Renderer Processes
+ * Called from Main Process to send Action to all Renderer Processes
  */
-const dispatchGlobally =
-  (action: Action, windows: Electron.BrowserWindow[]) => {
-    windows.forEach(window =>
-      window.webContents.send(GLOBAL_ACTION_MESSAGE, extractAction(action))
+const sendToAllWindows = (action: GlobalAction) =>
+  BrowserWindow.getAllWindows().forEach(window =>
+    // Send action through IPC to window, where middleware will dispatch to store
+    window.webContents.send(GLOBAL_ACTION_MESSAGE, extractAction(action))
+  )
+
+/**
+ * Main Process Middleware dispatching Global Actions to all Windows
+ */
+const mainMiddleware =
+  ({ dispatch }: MiddlewareAPI<any>) => {
+
+    // Listen for Global Actions dispatched to the Process
+    ipcMain.on(GLOBAL_ACTION_MESSAGE, (_, action: GlobalAction) =>
+      dispatch(action)
     )
+
+    return (next: Dispatch<any>) => (action: GlobalAction) => {
+      if (isGlobalAction(action))
+        sendToAllWindows(action)
+
+      return next(action)
+    }
   }
 
 /**
- * Creates a Redux Middleware for Main Process to dispatch
- * a Global Action between all Electron Processes
+ * Renderer Process Middleware dispatching Global Actions to Main Process
  */
-export const createGlobalDispatchMainMiddleware =
-  <S>(getWindows: (state?: S) => Electron.BrowserWindow[]): Middleware =>
-    ({ dispatch, getState }: MiddlewareAPI<S>) => {
-
-      // Listen for Global Actions dispatched to the Process
-      ipcMain.on(GLOBAL_ACTION_MESSAGE, (_, action: Action) =>
-        dispatch(action)
-      )
-
-      return (next: Dispatch<S>): Dispatch<S> => (action: GlobalAction) => {
-        if (isActionGlobal(action))
-          dispatchGlobally(action, getWindows(getState()))
-
-        return next(action)
-      }
-    }
-
-/**
- * Redux Middleware for Renderer Process to automatically dispatch
- * a Global Action between all Electron processes
- */
-export const globalDispatchRendererMiddleware: Middleware =
+const rendererMiddleware: Middleware =
   ({ dispatch }: MiddlewareAPI<any>) => {
 
     // Listen for Global Actions dispatched to the Process
@@ -68,7 +66,9 @@ export const globalDispatchRendererMiddleware: Middleware =
     )
 
     return (next: Dispatch<any>) => (action: Action) =>
-      isActionGlobal(action)
+      isGlobalAction(action)
         ? ipcRenderer.send(GLOBAL_ACTION_MESSAGE, action)
         : next(action)
   }
+
+export default isRenderer ? rendererMiddleware : mainMiddleware
